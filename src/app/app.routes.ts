8b5e1c321defece6c20f6436113d24dbdecf863d
Rodrigo Acosta <jracosta1991@gmail.com>
2025-07-05 16:57:36 -0300

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./features/blog/pages/blog.component').then((m) => m.BlogPage),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/pages/contact.component').then(
+        (m) => m.ContactPage
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
